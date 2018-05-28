da08cb89f1d6ee540e2c1da97119ce2d7f7b60a1
Mateusz Kaczmarek <mateuszk131@gmail.com>
2018-05-29 00:38:22 +0200

    start

--- Szyfry/Form1.Designer.cs
+++ Szyfry/Form1.Designer.cs
@@ -28,13 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.radioButtonMD5 = new System.Windows.Forms.RadioButton();
+            this.richTextBoxInput = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxCiphered = new System.Windows.Forms.RichTextBox();
+            this.radioButtonSha1 = new System.Windows.Forms.RadioButton();
+            this.radioButtonSha256 = new System.Windows.Forms.RadioButton();
+            this.radioButtonSha384 = new System.Windows.Forms.RadioButton();
+            this.radioButtonSha512 = new System.Windows.Forms.RadioButton();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.richTextBoxLoadCiphered = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxDecyphered = new System.Windows.Forms.RichTextBox();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // radioButtonMD5
+            // 
+            this.radioButtonMD5.AutoSize = true;
+            this.radioButtonMD5.Location = new System.Drawing.Point(331, 88);
+            this.radioButtonMD5.Name = "radioButtonMD5";
+            this.radioButtonMD5.Size = new System.Drawing.Size(48, 17);
+            this.radioButtonMD5.TabIndex = 0;
+            this.radioButtonMD5.Text = "MD5";
+            this.radioButtonMD5.UseVisualStyleBackColor = true;
+            this.radioButtonMD5.CheckedChanged += new System.EventHandler(this.radioButtonMD5_CheckedChanged);
+            // 
+            // richTextBoxInput
+            // 
+            this.richTextBoxInput.Location = new System.Drawing.Point(13, 88);
+            this.richTextBoxInput.Name = "richTextBoxInput";
+            this.richTextBoxInput.Size = new System.Drawing.Size(301, 110);
+            this.richTextBoxInput.TabIndex = 1;
+            this.richTextBoxInput.Text = "";
+            this.richTextBoxInput.TextChanged += new System.EventHandler(this.richTextBoxInput_TextChanged);
+            // 
+            // richTextBoxCiphered
+            // 
+            this.richTextBoxCiphered.Location = new System.Drawing.Point(417, 88);
+            this.richTextBoxCiphered.Name = "richTextBoxCiphered";
+            this.richTextBoxCiphered.ReadOnly = true;
+            this.richTextBoxCiphered.Size = new System.Drawing.Size(310, 110);
+            this.richTextBoxCiphered.TabIndex = 2;
+            this.richTextBoxCiphered.Text = "";
+            // 
+            // radioButtonSha1
+            // 
+            this.radioButtonSha1.AutoSize = true;
+            this.radioButtonSha1.Location = new System.Drawing.Point(331, 112);
+            this.radioButtonSha1.Name = "radioButtonSha1";
+            this.radioButtonSha1.Size = new System.Drawing.Size(50, 17);
+            this.radioButtonSha1.TabIndex = 3;
+            this.radioButtonSha1.Text = "Sha1";
+            this.radioButtonSha1.UseVisualStyleBackColor = true;
+            this.radioButtonSha1.CheckedChanged += new System.EventHandler(this.radioButtonSha1_CheckedChanged);
+            // 
+            // radioButtonSha256
+            // 
+            this.radioButtonSha256.AutoSize = true;
+            this.radioButtonSha256.Location = new System.Drawing.Point(329, 135);
+            this.radioButtonSha256.Name = "radioButtonSha256";
+            this.radioButtonSha256.Size = new System.Drawing.Size(62, 17);
+            this.radioButtonSha256.TabIndex = 4;
+            this.radioButtonSha256.Text = "Sha256";
+            this.radioButtonSha256.UseVisualStyleBackColor = true;
+            this.radioButtonSha256.CheckedChanged += new System.EventHandler(this.radioButtonSha256_CheckedChanged);
+            // 
+            // radioButtonSha384
+            // 
+            this.radioButtonSha384.AutoSize = true;
+            this.radioButtonSha384.Location = new System.Drawing.Point(329, 158);
+            this.radioButtonSha384.Name = "radioButtonSha384";
+            this.radioButtonSha384.Size = new System.Drawing.Size(62, 17);
+            this.radioButtonSha384.TabIndex = 5;
+            this.radioButtonSha384.Text = "Sha384";
+            this.radioButtonSha384.UseVisualStyleBackColor = true;
+            this.radioButtonSha384.CheckedChanged += new System.EventHandler(this.radioButtonSha384_CheckedChanged);
+            // 
+            // radioButtonSha512
+            // 
+            this.radioButtonSha512.AutoSize = true;
+            this.radioButtonSha512.Location = new System.Drawing.Point(329, 181);
+            this.radioButtonSha512.Name = "radioButtonSha512";
+            this.radioButtonSha512.Size = new System.Drawing.Size(62, 17);
+            this.radioButtonSha512.TabIndex = 6;
+            this.radioButtonSha512.Text = "Sha512";
+            this.radioButtonSha512.UseVisualStyleBackColor = true;
+            this.radioButtonSha512.CheckedChanged += new System.EventHandler(this.radioButtonSha512_CheckedChanged);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(652, 204);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 7;
+            this.buttonSave.Text = "Zapisz";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // richTextBoxLoadCiphered
+            // 
+            this.richTextBoxLoadCiphered.Location = new System.Drawing.Point(13, 264);
+            this.richTextBoxLoadCiphered.Name = "richTextBoxLoadCiphered";
+            this.richTextBoxLoadCiphered.ReadOnly = true;
+            this.richTextBoxLoadCiphered.Size = new System.Drawing.Size(310, 110);
+            this.richTextBoxLoadCiphered.TabIndex = 8;
+            this.richTextBoxLoadCiphered.Text = "";
+            this.richTextBoxLoadCiphered.TextChanged += new System.EventHandler(this.richTextBoxLoadCiphered_TextChanged);
+            // 
+            // richTextBoxDecyphered
+            // 
+            this.richTextBoxDecyphered.Location = new System.Drawing.Point(417, 264);
+            this.richTextBoxDecyphered.Name = "richTextBoxDecyphered";
+            this.richTextBoxDecyphered.Size = new System.Drawing.Size(301, 110);
+            this.richTextBoxDecyphered.TabIndex = 9;
+            this.richTextBoxDecyphered.Text = "";
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(13, 381);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 10;
+            this.buttonLoad.Text = "Wczytaj";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.richTextBoxDecyphered);
+            this.Controls.Add(this.richTextBoxLoadCiphered);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.radioButtonSha512);
+            this.Controls.Add(this.radioButtonSha384);
+            this.Controls.Add(this.radioButtonSha256);
+            this.Controls.Add(this.radioButtonSha1);
+            this.Controls.Add(this.richTextBoxCiphered);
+            this.Controls.Add(this.richTextBoxInput);
+            this.Controls.Add(this.radioButtonMD5);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton radioButtonMD5;
+        private System.Windows.Forms.RichTextBox richTextBoxInput;
+        private System.Windows.Forms.RichTextBox richTextBoxCiphered;
+        private System.Windows.Forms.RadioButton radioButtonSha1;
+        private System.Windows.Forms.RadioButton radioButtonSha256;
+        private System.Windows.Forms.RadioButton radioButtonSha384;
+        private System.Windows.Forms.RadioButton radioButtonSha512;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.RichTextBox richTextBoxLoadCiphered;
+        private System.Windows.Forms.RichTextBox richTextBoxDecyphered;
+        private System.Windows.Forms.Button buttonLoad;
     }
 }
 
--- Szyfry/Form1.cs
+++ Szyfry/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
 
 namespace Szyfry
 {
@@ -16,5 +17,154 @@ namespace Szyfry
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        public static string MD5decypher(string s)
+        {
+            using (var provider = System.Security.Cryptography.MD5.Create())
+            {
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in provider.ComputeHash(Encoding.UTF8.GetBytes(s)))
+                    builder.Append(b.ToString("x2").ToLower());
+                return builder.ToString();
+            }
+        }
+        #region szyfry
+        public static string MD5(string s)
+        {
+            using (var provider = System.Security.Cryptography.MD5.Create())
+            {
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in provider.ComputeHash(Encoding.UTF8.GetBytes(s)))
+                    builder.Append(b.ToString("x2").ToLower());
+                return builder.ToString();
+            }
+        }
+        public static string Sha1(string s)
+        {
+            using (var provider = System.Security.Cryptography.SHA1.Create())
+            {
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in provider.ComputeHash(Encoding.UTF8.GetBytes(s)))
+                    builder.Append(b.ToString("x2").ToLower());
+                return builder.ToString();
+            }
+        }
+        public static string Sha256(string s)
+        {
+            using (var provider = System.Security.Cryptography.SHA256.Create())
+            {
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in provider.ComputeHash(Encoding.UTF8.GetBytes(s)))
+                    builder.Append(b.ToString("x2").ToLower());
+                return builder.ToString();
+            }
+        }
+        public static string Sha384(string s)
+        {
+            using (var provider = System.Security.Cryptography.SHA384.Create())
+            {
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in provider.ComputeHash(Encoding.UTF8.GetBytes(s)))
+                    builder.Append(b.ToString("x2").ToLower());
+                return builder.ToString();
+            }
+        }
+        public static string Sha512(string s)
+        {
+            using (var provider = System.Security.Cryptography.SHA512.Create())
+            {
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in provider.ComputeHash(Encoding.UTF8.GetBytes(s)))
+                    builder.Append(b.ToString("x2").ToLower());
+                return builder.ToString();
+            }
+        }
+        #endregion
+        #region radio button check
+        private void radioButtonMD5_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBoxCiphered.Text = MD5(richTextBoxInput.Text);
+        }
+        private void radioButtonSha1_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBoxCiphered.Text = Sha1(richTextBoxInput.Text);
+        }
+        private void radioButtonSha256_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBoxCiphered.Text = Sha256(richTextBoxInput.Text);
+        }
+        private void radioButtonSha384_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBoxCiphered.Text = Sha384(richTextBoxInput.Text);
+        }
+        private void radioButtonSha512_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBoxCiphered.Text = Sha512(richTextBoxInput.Text);
+        }
+        #endregion
+        private void richTextBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            if (radioButtonMD5.Checked)
+            {
+                richTextBoxCiphered.Text = MD5(richTextBoxInput.Text);
+            }
+            if (radioButtonSha1.Checked)
+            {
+                richTextBoxCiphered.Text = Sha1(richTextBoxInput.Text);
+            }
+            if (radioButtonSha256.Checked)
+            {
+                richTextBoxCiphered.Text = Sha256(richTextBoxInput.Text);
+            }
+            if (radioButtonSha384.Checked)
+            {
+                richTextBoxCiphered.Text = Sha384(richTextBoxInput.Text);
+            }
+            if (radioButtonSha512.Checked)
+            {
+                richTextBoxCiphered.Text = Sha512(richTextBoxInput.Text);
+            }
+        }
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (radioButtonMD5.Checked)
+            {
+                System.IO.File.WriteAllText(@"MD5.txt", richTextBoxCiphered.Text + Environment.NewLine);
+            }
+            if (radioButtonSha1.Checked)
+            {
+                System.IO.File.WriteAllText(@"Sha1.txt", richTextBoxCiphered.Text + Environment.NewLine);
+            }
+            if (radioButtonSha256.Checked)
+            {
+                System.IO.File.WriteAllText(@"Sha256.txt", richTextBoxCiphered.Text + Environment.NewLine);
+            }
+            if (radioButtonSha384.Checked)
+            {
+                System.IO.File.WriteAllText(@"Sha384.txt", richTextBoxCiphered.Text + Environment.NewLine);
+            }
+            if (radioButtonSha512.Checked)
+            {
+                System.IO.File.WriteAllText(@"Sha512.txt", richTextBoxCiphered.Text + Environment.NewLine);
+            }
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            if (radioButtonMD5.Checked)
+            {
+                richTextBoxLoadCiphered.LoadFile(@"MD5.txt", RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        private void richTextBoxLoadCiphered_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
     }
 }
+
+
+
