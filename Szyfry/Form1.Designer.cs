c17b96f00cbc5bce97426cbffdf4fa1c7ae123a3
Mateusz Kaczmarek <mateuszk131@gmail.com>
2018-05-29 12:07:55 +0200

    groupboxy

--- Szyfry/Form1.Designer.cs
+++ Szyfry/Form1.Designer.cs
@@ -57,13 +57,17 @@
             this.buttonLoadClear = new System.Windows.Forms.Button();
             this.buttonClearLoad = new System.Windows.Forms.Button();
             this.dokumentacjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // radioButtonMD5
             // 
             this.radioButtonMD5.AutoSize = true;
-            this.radioButtonMD5.Location = new System.Drawing.Point(331, 88);
+            this.radioButtonMD5.Location = new System.Drawing.Point(6, 24);
             this.radioButtonMD5.Name = "radioButtonMD5";
             this.radioButtonMD5.Size = new System.Drawing.Size(48, 17);
             this.radioButtonMD5.TabIndex = 0;
@@ -75,7 +79,7 @@
             // 
             this.richTextBoxInput.Location = new System.Drawing.Point(13, 88);
             this.richTextBoxInput.Name = "richTextBoxInput";
-            this.richTextBoxInput.Size = new System.Drawing.Size(301, 110);
+            this.richTextBoxInput.Size = new System.Drawing.Size(310, 110);
             this.richTextBoxInput.TabIndex = 1;
             this.richTextBoxInput.Text = "";
             this.richTextBoxInput.TextChanged += new System.EventHandler(this.richTextBoxInput_TextChanged);
@@ -93,7 +97,7 @@
             // radioButtonSha1
             // 
             this.radioButtonSha1.AutoSize = true;
-            this.radioButtonSha1.Location = new System.Drawing.Point(331, 112);
+            this.radioButtonSha1.Location = new System.Drawing.Point(6, 48);
             this.radioButtonSha1.Name = "radioButtonSha1";
             this.radioButtonSha1.Size = new System.Drawing.Size(53, 17);
             this.radioButtonSha1.TabIndex = 3;
@@ -104,7 +108,7 @@
             // radioButtonSha256
             // 
             this.radioButtonSha256.AutoSize = true;
-            this.radioButtonSha256.Location = new System.Drawing.Point(329, 135);
+            this.radioButtonSha256.Location = new System.Drawing.Point(4, 71);
             this.radioButtonSha256.Name = "radioButtonSha256";
             this.radioButtonSha256.Size = new System.Drawing.Size(65, 17);
             this.radioButtonSha256.TabIndex = 4;
@@ -115,7 +119,7 @@
             // radioButtonSha384
             // 
             this.radioButtonSha384.AutoSize = true;
-            this.radioButtonSha384.Location = new System.Drawing.Point(329, 158);
+            this.radioButtonSha384.Location = new System.Drawing.Point(4, 94);
             this.radioButtonSha384.Name = "radioButtonSha384";
             this.radioButtonSha384.Size = new System.Drawing.Size(62, 17);
             this.radioButtonSha384.TabIndex = 5;
@@ -126,7 +130,7 @@
             // radioButtonSha512
             // 
             this.radioButtonSha512.AutoSize = true;
-            this.radioButtonSha512.Location = new System.Drawing.Point(329, 181);
+            this.radioButtonSha512.Location = new System.Drawing.Point(4, 117);
             this.radioButtonSha512.Name = "radioButtonSha512";
             this.radioButtonSha512.Size = new System.Drawing.Size(62, 17);
             this.radioButtonSha512.TabIndex = 6;
@@ -146,7 +150,7 @@
             // 
             // richTextBoxLoadCiphered
             // 
-            this.richTextBoxLoadCiphered.Location = new System.Drawing.Point(13, 264);
+            this.richTextBoxLoadCiphered.Location = new System.Drawing.Point(13, 235);
             this.richTextBoxLoadCiphered.Name = "richTextBoxLoadCiphered";
             this.richTextBoxLoadCiphered.ReadOnly = true;
             this.richTextBoxLoadCiphered.Size = new System.Drawing.Size(310, 110);
@@ -156,7 +160,7 @@
             // 
             // buttonLoad
             // 
-            this.buttonLoad.Location = new System.Drawing.Point(125, 415);
+            this.buttonLoad.Location = new System.Drawing.Point(124, 393);
             this.buttonLoad.Name = "buttonLoad";
             this.buttonLoad.Size = new System.Drawing.Size(83, 23);
             this.buttonLoad.TabIndex = 10;
@@ -168,7 +172,7 @@
             // 
             this.labelPorownanie.AutoSize = true;
             this.labelPorownanie.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.labelPorownanie.Location = new System.Drawing.Point(329, 264);
+            this.labelPorownanie.Location = new System.Drawing.Point(329, 235);
             this.labelPorownanie.Name = "labelPorownanie";
             this.labelPorownanie.Size = new System.Drawing.Size(70, 25);
             this.labelPorownanie.TabIndex = 11;
@@ -178,7 +182,7 @@
             // radioButtonSHA512Load
             // 
             this.radioButtonSHA512Load.AutoSize = true;
-            this.radioButtonSHA512Load.Location = new System.Drawing.Point(264, 380);
+            this.radioButtonSHA512Load.Location = new System.Drawing.Point(259, 19);
             this.radioButtonSHA512Load.Name = "radioButtonSHA512Load";
             this.radioButtonSHA512Load.Size = new System.Drawing.Size(65, 17);
             this.radioButtonSHA512Load.TabIndex = 19;
@@ -188,7 +192,7 @@
             // radioButtonSHA384Load
             // 
             this.radioButtonSHA384Load.AutoSize = true;
-            this.radioButtonSHA384Load.Location = new System.Drawing.Point(196, 380);
+            this.radioButtonSHA384Load.Location = new System.Drawing.Point(191, 19);
             this.radioButtonSHA384Load.Name = "radioButtonSHA384Load";
             this.radioButtonSHA384Load.Size = new System.Drawing.Size(65, 17);
             this.radioButtonSHA384Load.TabIndex = 18;
@@ -198,7 +202,7 @@
             // radioButtonSHA256Load
             // 
             this.radioButtonSHA256Load.AutoSize = true;
-            this.radioButtonSHA256Load.Location = new System.Drawing.Point(125, 380);
+            this.radioButtonSHA256Load.Location = new System.Drawing.Point(120, 19);
             this.radioButtonSHA256Load.Name = "radioButtonSHA256Load";
             this.radioButtonSHA256Load.Size = new System.Drawing.Size(65, 17);
             this.radioButtonSHA256Load.TabIndex = 17;
@@ -208,7 +212,7 @@
             // radioButtonSHA1Load
             // 
             this.radioButtonSHA1Load.AutoSize = true;
-            this.radioButtonSHA1Load.Location = new System.Drawing.Point(66, 380);
+            this.radioButtonSHA1Load.Location = new System.Drawing.Point(61, 19);
             this.radioButtonSHA1Load.Name = "radioButtonSHA1Load";
             this.radioButtonSHA1Load.Size = new System.Drawing.Size(53, 17);
             this.radioButtonSHA1Load.TabIndex = 16;
@@ -218,7 +222,7 @@
             // radioButtonMD5Load
             // 
             this.radioButtonMD5Load.AutoSize = true;
-            this.radioButtonMD5Load.Location = new System.Drawing.Point(12, 380);
+            this.radioButtonMD5Load.Location = new System.Drawing.Point(7, 19);
             this.radioButtonMD5Load.Name = "radioButtonMD5Load";
             this.radioButtonMD5Load.Size = new System.Drawing.Size(48, 17);
             this.radioButtonMD5Load.TabIndex = 15;
@@ -246,7 +250,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 248);
+            this.label3.Location = new System.Drawing.Point(9, 219);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(180, 13);
             this.label3.TabIndex = 22;
@@ -327,7 +331,7 @@
             // 
             this.buttonClearLoad.BackColor = System.Drawing.SystemColors.Control;
             this.buttonClearLoad.Image = global::Szyfry.Properties.Resources.error1;
-            this.buttonClearLoad.Location = new System.Drawing.Point(300, 349);
+            this.buttonClearLoad.Location = new System.Drawing.Point(300, 320);
             this.buttonClearLoad.Name = "buttonClearLoad";
             this.buttonClearLoad.Size = new System.Drawing.Size(23, 25);
             this.buttonClearLoad.TabIndex = 12;
@@ -341,19 +345,44 @@
             this.dokumentacjaToolStripMenuItem.Text = "Dokumentacja";
             this.dokumentacjaToolStripMenuItem.Click += new System.EventHandler(this.dokumentacjaToolStripMenuItem_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonSha1);
+            this.groupBox1.Controls.Add(this.radioButtonMD5);
+            this.groupBox1.Controls.Add(this.radioButtonSha256);
+            this.groupBox1.Controls.Add(this.radioButtonSha384);
+            this.groupBox1.Controls.Add(this.radioButtonSha512);
+            this.groupBox1.Location = new System.Drawing.Point(329, 72);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(82, 140);
+            this.groupBox1.TabIndex = 24;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Szyfrowanie";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButtonMD5Load);
+            this.groupBox2.Controls.Add(this.radioButtonSHA1Load);
+            this.groupBox2.Controls.Add(this.radioButtonSHA256Load);
+            this.groupBox2.Controls.Add(this.radioButtonSHA384Load);
+            this.groupBox2.Controls.Add(this.radioButtonSHA512Load);
+            this.groupBox2.Location = new System.Drawing.Point(13, 351);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(329, 40);
+            this.groupBox2.TabIndex = 25;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Szyfrowanie";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.radioButtonSHA512Load);
-            this.Controls.Add(this.radioButtonSHA384Load);
-            this.Controls.Add(this.radioButtonSHA256Load);
-            this.Controls.Add(this.radioButtonSHA1Load);
-            this.Controls.Add(this.radioButtonMD5Load);
             this.Controls.Add(this.buttonDecypheredClear);
             this.Controls.Add(this.buttonLoadClear);
             this.Controls.Add(this.buttonClearLoad);
@@ -361,13 +390,8 @@
             this.Controls.Add(this.buttonLoad);
             this.Controls.Add(this.richTextBoxLoadCiphered);
             this.Controls.Add(this.buttonSave);
-            this.Controls.Add(this.radioButtonSha512);
-            this.Controls.Add(this.radioButtonSha384);
-            this.Controls.Add(this.radioButtonSha256);
-            this.Controls.Add(this.radioButtonSha1);
             this.Controls.Add(this.richTextBoxCiphered);
             this.Controls.Add(this.richTextBoxInput);
-            this.Controls.Add(this.radioButtonMD5);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
@@ -375,6 +399,10 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -411,6 +439,8 @@
         private System.Windows.Forms.ToolStripMenuItem celProjektuToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem oAutorzeToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem dokumentacjaToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
